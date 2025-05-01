19a1943010c7f771d681161aa532676fca17071c
Darkfinder24k <shivikush77@gmail.com>
2025-05-01 21:13:26 +0530

    Update api.py

--- api.py
+++ api.py
@@ -1,2 +1,4 @@
 gemini_api = 'AIzaSyAbXv94hwzhbrxhBYq-zS58LkhKZQ6cjMg'
 stability_api = 'sk-fIgUuEXIH2yREpTnMoNkO3IV0zhXQhqwPxDXFa29tCiiCGUD'
+llama_api = "e5b7931e7e214e1eb43ba7182d7a2176"
+llama_endpoint = "https://api.aimlapi.com/v1"  # Example endpoint
