071cf5ca5eee43bfa8637699d3e028d62cc319a9
Darkfinder24k <shivikush77@gmail.com>
2025-05-06 20:37:51 +0530

    Update api.py

--- api.py
+++ api.py
@@ -2,4 +2,4 @@ gemini_api = 'AIzaSyAbXv94hwzhbrxhBYq-zS58LkhKZQ6cjMg'
 stability_api = 'sk-fIgUuEXIH2yREpTnMoNkO3IV0zhXQhqwPxDXFa29tCiiCGUD'
 llama_api = "e5b7931e7e214e1eb43ba7182d7a2176"
 llama_endpoint = "https://api.aimlapi.com/v1/chat/completions"  # Example endpoint
-deepseek_api = "sk-2ddfda92d65b41eaba5d26c1d597d671"
+deepseek_api = "sk-c4cdf027b91848d7af386bd342c3f61f"
